4b32521e8697220d214be978e18ea1f0531deee0
Berg <jannik.berg.tp@tomra.com>
2023-09-24 23:22:54 +0200

    added simple aliases for git commands

--- .config/fish/alias.fish
+++ .config/fish/alias.fish
@@ -8,6 +8,10 @@ alias glogin="gcloud auth login && gcloud auth application-default login"
 alias mvnp="mvn clean install -Dpackaging=true"
 alias mvndc="mvn dependency-check:aggregate -U -DcveValidForHours=24 --fail-at-end"
 
+# Git
+alias gcm="git checkout main"
+alias gcb="git checkout $1"
+alias gcnb="git checkout -b $1"
 
 # Java
 alias java11='export JAVA_HOME=$JAVA_11_HOME'
